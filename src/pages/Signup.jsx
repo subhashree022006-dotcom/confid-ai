e04710fb517cccd8004ad5c14da26d0d5920b16d
Subhashree S <subhashree022006@gmail.com>
2026-08-24 18:26:39 +0530

    Fix Signup.jsx duplicate code

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,3 @@
-
-import Pricing from "./pages/Pricing.jsx";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
